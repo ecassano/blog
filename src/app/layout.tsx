ab18814f83c1668f814eeaf1ff43e714950991a2
ecassano <eduardocassanodesa@uol.com.br>
2025-10-28 14:19:10 -0300

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="bg-amber-400">
+          {children}
+        </div>
       </body>
     </html>
   );
